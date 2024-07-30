5b311288272dc27b26d4cf16575ead2d2263dce1
Mica Lorraine D. Porras <micalorrainedporras@iskolarngbayan.pup.edu.ph>
2024-07-30 21:52:56 +0800

    update todo.jsx
    can add and delete task

--- to-do/src/components/todo/Todo.jsx
+++ to-do/src/components/todo/Todo.jsx
@@ -1,20 +1,59 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 
 const Todo = () => {
-  const [todos, setTodos] = useState([{task : "take a nap", completed: false, bookmarked: false}, {task : "create an app", completed: true, bookmarked: true}])
+  
+  function getStoredTodos() {
+    let data = localStorage.getItem("todos")
+    let json = JSON.parse(data)
 
+    if (json) {
+      return json
+    } 
+    return []
+  }
+
+  const [todos, setTodos] = useState(getStoredTodos())
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   function AddTask(event) {
     event.preventDefault()
 
     let task = event.target.task.value
 
-    alert ("new task: " + task)
+    if (!task) {
+        alert("Please input a task")
+        return
+    }
+
+    setTodos([...todos, {task:task, completed: false}])
+
+    alert("New task added")
 
     event.target.reset()
     
     
+  }
+
+  function changeTaskStatus(index) {
+    let newTodos = [...todos]
+    newTodos[index].completed = !newTodos[index].completed
+    setTodos(newTodos)
+  }
+
+  function changeBookmarkStatus(index) {
+    let newTodos = [...todos]
+    newTodos[index].bookmarked = !newTodos[index].bookmarked
+    setTodos(newTodos)
+  }
+
+  function deleteTask(index) {
+    let newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
   }
   
   return (
@@ -24,7 +63,7 @@ const Todo = () => {
           {/* <p className='text-white mb-5'>TODO APP</p> */}
 
           <form className="d-flex mb-5" onSubmit={AddTask}>
-            <input className="form-control me-2" placeholder="Task" name='task'/>
+            <input className="form-control me-2" placeholder="Add a Task" name='task'/>
             <button className="btn btn-outline-light me-2" type="submit"><i className="bi bi-plus-lg"></i></button>
             <button className="btn btn-outline-light"><i className="bi bi-bookmarks-fill"></i></button>
           </form>
@@ -35,15 +74,17 @@ const Todo = () => {
                 <div key={index} className='rounded mt-2 p-2 d-flex' style={{backgroundColor: todo.completed ? "#FF7F4D" : "transparent", 
                 border: todo.completed ? "solid white 1px" : "solid white 2px",
                 color: "white"}}>
-                  <div className='me-auto'>
-                    <i className= {"me-2 h5 " + (todo.completed ? "bi bi-check-square-fill" : "bi bi-square")}></i>
+                  <div className='me-auto' style={{cursor: "pointer"}}>
+                    <i className= {"me-2 h5 " + (todo.completed ? "bi bi-check-square-fill" : "bi bi-square")} style={{cursor: "pointer"}}
+                    onClick={() => changeTaskStatus(index)}></i>
                     {todo.task}
                   </div>
 
                   <div>
-                    <i className= {"me-2 h5 " + (todo.bookmarked ? "bi bi-bookmark-fill me-2 h5" : "bi bi-bookmark me-2 h5")}></i>
-                    <i className="bi bi-pencil-square me-2 h5"></i>
-                    <i className="bi bi-trash3 h5"></i>
+                    <i className= {"me-2 h5 " + (todo.bookmarked ? "bi bi-bookmark-fill me-2 h5" : "bi bi-bookmark me-2 h5")} style={{cursor: "pointer"}}
+                    onClick={() => changeBookmarkStatus(index)}></i>
+                    <i className="bi bi-pencil-square me-2 h5" style={{cursor: "pointer"}}></i>
+                    <i className="bi bi-trash3 h5" style={{cursor: "pointer"}} onClick={() => deleteTask(index)}></i>
                   </div>
                 </div>
               )
